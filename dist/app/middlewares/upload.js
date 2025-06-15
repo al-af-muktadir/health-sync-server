50eb62c707eb941c69bde14a6d2c6272107cb3be
al-af-muktadir <al.afmuktadir.28@gmail.com>
2025-06-16 02:14:10 +0600

    kjjj

--- dist/app/middlewares/upload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// import multer from "multer";
-// export const uploadSingleImage = multer({
-//   storage: multer.memoryStorage(), // <── stays in RAM
-//   limits: { fileSize: 4 * 1024 * 1024 }, //   4 MB hard stop
-// }).single("profilePhoto");
--- dist/app/modules/User/User.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QueriesFields = exports.searchableFields = void 0;
-exports.searchableFields = ["name", "email", "contactNumber"];
-exports.QueriesFields = ["name", "email", "contactNumber", "searchTerm"];
